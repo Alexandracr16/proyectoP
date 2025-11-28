55a926c148a69440b1f20b569a1d9563c12364a2
unknown <jescastellanos@uniboyaca.edu.co>
2025-11-28 14:20:46 -0500

    creacion segundo microservicio

---
+++ backend/vuelos/app/Models/Vuelo.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Vuelo extends Model
+{
+    protected $table = 'flights';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nave_id',
+        'origin',
+        'destination',
+        'departure',
+        'arrival',
+        'price'
+    ];
+
+    // Relación: un vuelo pertenece a una nave
+    public function nave()
+    {
+        return $this->belongsTo(Nave::class, 'nave_id', 'id');
+    }
+
+    // Un vuelo puede tener muchas reservas
+    public function reservas()
+    {
+        return $this->hasMany(Reserva::class, 'flight_id', 'id');
+    }
+}
